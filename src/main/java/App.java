ec48f6fd972664140d69e471997937d13870c30a
Robert Muthuri <muthuri.r@gmail.com>
2019-09-09 10:52:53 +0300

    Adds an index.hbs and a root route for it in App.java.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,6 @@
 import java.util.HashMap;
+import java.util.Map;
+
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 import static spark.Spark.*;
@@ -7,5 +9,9 @@ public class App {
     public static void main(String[] args) {
         staticFileLocation("/public");
 
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
     }
 }

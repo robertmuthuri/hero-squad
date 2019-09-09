f603ec53c43bcb4322e880c1aa2f37e41fb9e1e2
Robert Muthuri <muthuri.r@gmail.com>
2019-09-09 11:07:24 +0300

    Adds an index.hbs to register a successful hero addition.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,7 @@
 import java.util.HashMap;
 import java.util.Map;
 
+import models.Hero;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 import static spark.Spark.*;
@@ -13,5 +14,13 @@ public class App {
             Map<String, Object> model = new HashMap<String, Object>();
             return new ModelAndView(model, "index.hbs");
         }, new HandlebarsTemplateEngine());
+
+        post("/heros/new", (request, response) -> { //URL to add a new hero on POST route
+            Map<String, Object> model = new HashMap<String, Object>();
+            String content = request.queryParams("hero");
+            Hero newHero = new Hero(content);
+            return new ModelAndView(model, "success.hbs");
+        }, new HandlebarsTemplateEngine());
     }
+
 }

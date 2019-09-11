9c9176b50ca8e824fb1096256b7992c1bd24e5d6
Robert Muthuri <muthuri.r@gmail.com>
2019-09-11 16:37:13 +0300

    Adds methods for CRUD behaviours to be executed by the interface.

--- src/main/java/dao/HeroDao.java
+++ src/main/java/dao/HeroDao.java
@@ -1,4 +1,25 @@
 package dao;
 
+import models.Hero;
+
+import java.awt.*;
+import java.util.List;
+
 public interface HeroDao {
+
+    //LIST
+    List<Hero> getAll();
+
+    //CREATE
+    void add(Hero hero);
+
+    //READ
+    Hero findById(int id);
+
+    //UPDATE
+    //void update(int id, String name);
+
+    //DELETE
+    //void deleteById(int id);
+    //void clearAllHeros();
 }

f6d0fb8f5f52fd40ddd754206830e4731658ff64
Robert Muthuri <muthuri.r@gmail.com>
2019-09-11 20:58:27 +0300

    Adds the methods to add, retrieve all and by id, in the Hero DAO class.

---
+++ src/main/java/dao/Sql2oHeroDao.java
@@ -0,0 +1,47 @@
+package dao;
+
+import models.Hero;
+import org.sql2o.*;
+import java.util.List;
+
+public class Sql2oHeroDao implements HeroDao {
+
+    private final Sql2o sql2o;
+
+    public Sql2oHeroDao(Sql2o sql2o) {
+        this.sql2o = sql2o; //makes the sql2o object available everywhere so we can call methods in it;
+    }
+
+    @Override
+    public void add(Hero hero) {
+        String sql = "INSERT INTO heroes (name, age, power, weakness) VALUES (:name, :age, :power, :weakness)"; // raw sql
+        try(Connection con = sql2o.open()) { //tries to open a connection
+            int id = (int) con.createQuery(sql, true) //make a new variable
+            .bind(hero) // map my argument onto the query so we can use information from it - means the object fields are mapped onto the parameters outlined in the SQL statement.
+            .executeUpdate() // run the query
+            .getKey(); //new row has been created so retrieve the key - int id is now the row number (row "key") of db
+            hero.setId(id); // update object to set id now from database
+        } catch (Sql2oException ex) {
+            System.out.println(ex); // oops we have an error!
+        }
+    }
+
+    @Override
+    public List<Hero> getAll() {
+        try (Connection con = sql2o.open()) {
+            return con.createQuery("SELECT * FROM heroes") //raw sql
+            .executeAndFetch(Hero.class); // fetch a list
+        }
+    }
+    @Override
+    public Hero findById(int id) {
+        try (Connection con = sql2o.open()) {
+            return con.createQuery("SELECT * FROM heroes WHERE id = :id") //raw sql
+            .addParameter("id", id) // key-value pair, key must match above.
+            .executeAndFetchFirst(Hero.class); // fetch an individual item
+        }
+    }
+
+
+
+}

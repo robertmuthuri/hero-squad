cdcb85053be70735b40fda00ba31375eee8f4b8b
Robert Muthuri <muthuri.r@gmail.com>
2019-09-14 09:12:30 -0700

    Adds the abstract methods to be implemented in the squad interface.

--- src/main/java/dao/SquadInterface.java
+++ src/main/java/dao/SquadInterface.java
@@ -1,4 +1,23 @@
 package dao;
 
+import models.Squad;
+
+import java.util.List;
+
 public interface SquadInterface {
+    //LIST
+    List<Squad> getAll();
+
+    //CREATE
+    void add(Squad squad);
+
+    //READ
+    Squad findById(int id);
+
+    //UPDATE
+    void update(int id, String name);
+
+    //DELETE
+    void deleteById(int id);
+    void deleteAllSquads();
 }

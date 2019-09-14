d0b2312c2140620816fd1c5a452e55c19c1bba25
Robert Muthuri <muthuri.r@gmail.com>
2019-09-14 08:30:25 -0700

    Adds constructor, setters, getters and equals and hashcode methods to the Category class.

--- src/main/java/models/Category.java
+++ src/main/java/models/Category.java
@@ -1,6 +1,31 @@
 package models;
 
+import java.util.Objects;
+
 public class Category {
     private String name;
     private int id;
+
+    public Category(String name) {
+        this.name = name;
+    }
+    // getters
+    public int getId() { return id; }
+    public String getName() { return name; }
+
+    // setters
+    public void setId(int id) { this.id = id; }
+    public void setName(String name) { this.name = name; }
+
+    //hashcode
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Category category = (Category) o;
+        return getId() == category.getId() &&
+                Objects.equals(getName(), category.getName());
+    }
+    @Override
+    public int hashCode() { return Objects.hash(getName(), getId()); }
 }
